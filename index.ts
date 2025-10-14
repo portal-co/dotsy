a4568cf0183fc323c3d0767e913cc5180c8f95db
Graham Kelly <gkgoat6700@gmail.com>
2025-10-13 23:19:09 -0400

    rendering and raw fixes

--- index.ts
+++ index.ts
@@ -9,6 +9,30 @@ export class Splice {
         yield* a(i[0]).#internal;
       }
     });
+  render = <T>(a: (t: TemplateStringsArray, ...args: any[]) => T): T => {
+    let newTemplate = [],
+      newArgs = [];
+    for (const item of this.#internal) {
+      if (typeof item === "string") {
+        if (newTemplate.length === newArgs.length + 1) {
+          newTemplate.push(item);
+        } else {
+          newTemplate[newTemplate.length - 1] += item;
+        }
+      } else {
+        newArgs.push(item[0]);
+        newTemplate.push("");
+      }
+    }
+    Object.defineProperty(newTemplate, "raw", {
+      configurable: false,
+      enumerable: true,
+      get() {
+        throw new TypeError("Template is not raw");
+      },
+    });
+    return a(newTemplate as any, ...newArgs);
+  };
   constructor(t: TemplateStringsArray, ...args: any[]) {
     let a: Splice_ = [];
     for (let i = 0; i < t.length; i++) {
@@ -30,7 +54,9 @@ export class Splice {
     seperator?: string;
   }): (t: TemplateStringsArray, ...args: Args) => T {
     return (t, ...args) => {
-      const raw = t.raw;
+      const raw = Object.getOwnPropertyDescriptor(t, "raw")!;
+      if ("get" in raw) raw.get = raw.get?.bind(t);
+      if ("set" in raw) raw.set = raw.set?.bind(t);
       const items = [...t];
       let newTemplate: string[] = [];
       let newArgs = [];
@@ -63,7 +89,7 @@ export class Splice {
         }
         if (i in args) newArgs.push(args[i]);
       }
-      (newTemplate as any).raw = raw;
+      Object.defineProperty(newTemplate, "raw", raw);
       return template(newTemplate as any, ...(newArgs as Args));
     };
   }
