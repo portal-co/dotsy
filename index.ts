1536f26cc7c889d77536fd32aff9f4b3df97abbc
Graham Kelly <gkgoat6700@gmail.com>
2025-10-13 21:57:09 -0400

    init

---
+++ index.ts
@@ -0,0 +1,58 @@
+export function dotsy<T, Args extends unknown[]>({
+  process,
+  template,
+  seperator = "//",
+}: {
+  process: (a: string) => (string | { value: any })[];
+  template: (t: TemplateStringsArray, ...args: Args) => T;
+  seperator?: string;
+}): (t: TemplateStringsArray, ...args: Args) => T {
+  return (t, ...args) => {
+    const raw = t.raw;
+    const items = [...t];
+    let newTemplate: string[] = [];
+    let newArgs = [];
+    let mode = false;
+    for (let i = 0; i < items.length; i++) {
+      const dotted = items[i].split(seperator);
+      for (const val of dotted) {
+        mode = !mode;
+        if (mode) {
+          if (newTemplate.length === newArgs.length + 1) {
+            newTemplate.push(val);
+          } else {
+            newTemplate[newTemplate.length - 1] += val;
+          }
+        } else {
+          const processed = process(val);
+          for (const item of processed) {
+            if (typeof item === "string") {
+              if (newTemplate.length === newArgs.length + 1) {
+                newTemplate.push(item);
+              } else {
+                newTemplate[newTemplate.length - 1] += item;
+              }
+            } else {
+              newArgs.push(item.value);
+              newTemplate.push("");
+            }
+          }
+        }
+      }
+      if (i in args) newArgs.push(args[i]);
+    }
+    (newTemplate as any).raw = raw;
+    return template(newTemplate as any, ...(newArgs as Args));
+  };
+}
+export function templ(
+  t: TemplateStringsArray,
+  ...args: any[]
+): (string | { value: any })[] {
+  let a: (string | { value: any })[] = [];
+  for (let i = 0; i < t.length; i++) {
+    a.push(t[i]);
+    if (i in args) a.push({ value: args[i] });
+  }
+  return a;
+}
